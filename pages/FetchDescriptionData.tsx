58453521821a6320643904ba07c80cdf855abd8b
yuutan1017 <n.you0611@gmail.com>
2023-01-07 22:45:11 +0900

    post for frontend

--- pages/FetchDescriptionData.tsx
+++ pages/FetchDescriptionData.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 type Description = {
-  id?: number;
+  id?: string;
   description: string;
 };
 
@@ -10,8 +10,7 @@ const BASE_URL = 'http://127.0.0.1:8000/api/description';
 
 export default function FetchDescriptionData(): JSX.Element {
   const [data, setData] = useState<Description[]>([]);
-  const [id, setId] = useState<number>(1);
-  const [post, setPost] = useState<Description>({ id: 0, description: '' });
+  const [post, setPost] = useState<Description>({ id: '', description: '' });
   const [updated, setUpdated] = useState<boolean>(false);
 
   const config = {
@@ -30,34 +29,16 @@ export default function FetchDescriptionData(): JSX.Element {
       id: post.id,
       description: post.description,
     };
-    await axios.put(`${BASE_URL}/${post.id}/`, _data, config).then((res) => {
-      if (res.data.id) {
-        setUpdated(!updated);
-        setPost({ id: 1, description: '' });
-      }
-    });
+    await axios
+      .put(`${BASE_URL}/${post.id}/`, _data, config)
+      .then((res) => {
+        if (res.data.id) {
+          setUpdated(!updated);
+          setPost({ id: '', description: '' });
+        }
+      })
+      .catch((error) => alert(error));
   };
-
-  // const createSkill = (arg: Skill) => {
-  //   const _data = {
-  //     title: arg.title,
-  //     color_code: arg.color_code,
-  //   };
-  //   axios
-  //     .post(BASE_URL, _data, {
-  //       headers: { 'Content-Type': 'application/json' },
-  //     })
-  //     .then((res) => {
-  //       setData([...data, res.data]);
-  //       setCreate({ title: '', color_code: '' });
-  //     });
-  // };
-
-  // const handleInputChange = (e: any) => {
-  //   const value = e.target.value;
-  //   const name = e.target.name;
-  //   setCreate({ ...create, [name]: value });
-  // };
 
   return (
     <div className="box-border m-5 text-xl">
@@ -76,12 +57,14 @@ export default function FetchDescriptionData(): JSX.Element {
         <input
           type="text"
           placeholder="id"
+          value={post.id}
           className="border border-black"
-          onChange={(e) => setPost({ ...post, id: parseInt(e.target.value) })}
+          onChange={(e) => setPost({ ...post, id: e.target.value })}
         />
         <input
           type="text"
           placeholder="description"
+          value={post.description}
           className="border border-black"
           onChange={(e) => setPost({ ...post, description: e.target.value })}
         />
---
+++ pages/FetchSkillData.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FetchSkillData():JSX.Element {
+  return (
+    <div>FetchSkillData</div>
+  )
+}
